4ab9ed4c4ec620aa9b34241357f9293152a8f1d9
Michael Klich <michal@michalklich.com>
2015-10-25 08:16:50 +0000

    Add loading json, output to datadog

--- pm2.py
+++ pm2.py
@@ -1,17 +1,28 @@
 #  PYTHONPATH=.:/usr/share/datadog/agent/ python checks.d/pm2.py
+import subprocess
+import json
 
 from checks import AgentCheck
 
 
-def run_command(command):
-    pass
+def load_json(command):
+    p = subprocess.Popen(command, stdout=subprocess.PIPE)
+    out = p.communicate()[0]
+    return json.loads(out)[0]
 
 
 class Pm2(AgentCheck):
 
     def check(self, instance):
-        json_data = run_command(instance['command'])
-        self.gauge('pm2.processes', json_data[''])
+        try:
+            json_data = load_json(instance['command'].split(' '))
+        except (ValueError, IndexError):
+            return
+        # cpu, memory, errors, processes, restart, online, status
+        self.gauge('pm2.processes.cpu', json_data['monit']['cpu'])
+        self.gauge('pm2.processes.memory', json_data['monit']['memory'])
+        self.gauge('pm2.processes.status', json_data['pm2_env']['status'])
+        self.gauge('pm2.processes.uptime', json_data['pm2_env']['pm_uptime'])
 
 
 if __name__ == '__main__':
